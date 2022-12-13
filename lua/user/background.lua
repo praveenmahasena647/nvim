7914a3a9d09af49fb47051078c0c81ec8e3182b0
praveenmahasena647 <115216588+praveenmahasena647@users.noreply.github.com>
2022-12-13 13:32:28 +0530

    backGround Update

--- lua/user/background.lua
+++ lua/user/background.lua
@@ -1,5 +1,14 @@
 function backGround()
-    vim.cmd(':! cd ~/Desktop/vue-me/waifu/ && bash main.sh')
+    local num = math.floor(math.random(1,3))
+    if num==1 then
+        vim.cmd(':!cd ~/Desktop/vue-me/waifu/  && bash waifu.sh')
+    end
+    if num==2 then
+        vim.cmd(':!cd ~/Desktop/vue-me/waifu/  && bash neko.sh')
+    end
+    if num==3 then
+        vim.cmd(':!cd ~/Desktop/vue-me/waifu/  && bash catboy.sh')
+    end
 end
 
 
